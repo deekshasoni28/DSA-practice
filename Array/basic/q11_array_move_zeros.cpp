b1a84e7cb306a8015fcce3610b0e994fea76daca
deekshasoni28 <soni.deeksha2806@gmail.com>
2026-04-14 20:08:08 +0530

    Move 0s in end

---
+++ Array/basic/q11_array_move_zeros.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[]={1,3,0,2,0,3,0,0,1,2,0,0,3};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int temp[n];
+    int j=0;
+    for (int i=0;i<n;i++){
+        if (arr[i]!=0){
+            temp[j]=arr[i];
+            j++;
+        }
+    }
+    while(j<n){
+        temp[j]=0;
+        j++;
+    }
+    cout<<"New array";
+    for (int i=0;i<n;i++){
+        cout<<temp[i]<<" ";
+    }
+    return 0;
+}
