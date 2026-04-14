d8c75eeeccc5765572ce83fce3219810ada42ac4
deekshasoni28 <soni.deeksha2806@gmail.com>
2026-04-14 23:08:26 +0530

    Key=sum of two number

---
+++ Array/leet_code_basic/q01_two_sum.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target){
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[i]+nums[j]==target){
+                    return {i,j};
+                }
+            }
+        }
+        return {};
+    }
+};
+
+int main(){
+    vector<int> nums = {2,7,11,15};
+    int target = 9;
+
+    Solution obj;
+    vector<int> ans = obj.twoSum(nums, target);
+
+    cout << ans[0] << " " << ans[1];
+
+    return 0;
+}
