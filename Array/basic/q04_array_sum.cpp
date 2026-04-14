32de6085c3af192e823163f17332c81e7f445e14
deekshasoni28 <soni.deeksha2806@gmail.com>
2026-04-14 19:59:38 +0530

    Sum

---
+++ Array/basic/q04_array_sum.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[]={1,2,3,4,5,6,7,8,9,10};
+        int n=sizeof(arr)/sizeof(arr[0]);
+        int sum=0;
+        for (int i =0;i<n;i++){
+            sum=sum+arr[i];
+        }
+        cout<<"Sum of array : "<<sum;
+        return 0;
+}
